16156c8efffe1f9f0c20877ef00ac59c2dd7bc2b
unknown <shreya.devops211@gmail.com>
2026-09-12 12:57:54 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "firstRg" {
+  name     = "shreya-rg"
+  location = "eastus"
+}
+
---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    source  = "hashicorp/azurerm"
+    version = "4.65.0"
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
