6b635855996ceccab0932408cd1c0ac6fed5def6
unknown <shreya.devops211@gmail.com>
2026-09-13 12:13:11 +0530

    Added the azurerm

--- provider.tf
+++ provider.tf
@@ -1,7 +1,9 @@
 terraform {
   required_providers {
-    source  = "hashicorp/azurerm"
-    version = "5.0.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.0"
+    }
   }
 }
 
